f43efc73518097ca83450c419fa5e2d7f283a371
Aaron Staugaard <astaugaard@icloud.com>
2025-03-02 09:41:32 -0500

    desktop files support and refactored configuration

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use std::{
-    collections::HashMap, fs, os::unix::process::CommandExt, path::PathBuf, process::Command,
+    collections::HashMap,
+    fs,
+    os::unix::process::CommandExt,
+    path::PathBuf,
+    process::{self, Command},
     rc::Rc,
 };
 
@@ -7,18 +11,19 @@ use clap::Parser;
 use gio::{
     glib::{Char, OptionArg, OptionFlags, Propagation},
     prelude::*,
-    ApplicationFlags,
+    ApplicationFlags, DesktopAppInfo,
 };
 use gtk4::{
-    gdk::{Display, Key},
+    gdk::{AppLaunchContext, Display, Key, Paintable, Texture},
     prelude::*,
-    Align, Button, CssProvider, EventControllerKey, GestureClick, Grid, Image, Label, Orientation,
-    Stack, Widget,
+    Align, Button, CssProvider, EventControllerKey, GestureClick, Grid, IconLookupFlags, IconTheme,
+    Image, Label, Orientation, Picture, Stack, TextDirection, Widget,
 };
 
 use gtk4_layer_shell::{Edge, Layer, LayerShell};
 
 use anyhow::{Context, Result};
+use once_cell::unsync::{Lazy, OnceCell};
 use serde::{Deserialize, Serialize};
 
 #[derive(Parser, Debug)]
@@ -32,33 +37,68 @@ struct Args {
 }
 
 #[derive(Serialize, Deserialize, Debug)]
+#[serde(untagged)]
 enum App {
     Folder {
         name: String,
+        icon: Option<String>,
+        image: Option<String>,
         applications: HashMap<String, App>,
     },
     Application {
-        command: String,
-        name: String,
-        icon: String,
+        command: Option<String>,
+        name: Option<String>,
+        icon: Option<String>,
+        image: Option<String>,
+        application: Option<String>,
     },
+}
+
+fn default_true() -> bool {
+    true
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Config {
     width: usize,
     height: usize,
+
+    icon_size: usize,
+
+    #[serde(default = "default_true")]
+    anchor_left: bool,
+
+    #[serde(default = "default_true")]
+    anchor_right: bool,
+
+    #[serde(default = "default_true")]
+    anchor_up: bool,
+
+    #[serde(default = "default_true")]
+    anchor_down: bool,
+
     applications: HashMap<String, App>,
 }
 
-#[derive(Debug, Clone)]
+type Name = String;
+
+#[derive(Clone)]
 enum VApp {
-    Folder(String, Rc<HashMap<Key, VApp>>),
-    Application(String, String, String),
+    Folder {
+        name: Name,
+        image: Paintable,
+        applications: Rc<HashMap<Key, VApp>>,
+    },
+    Application {
+        name: Name,
+        image: Paintable,
+        command: Rc<dyn Fn() -> Result<()>>,
+    },
 }
 
 struct VConfig {
     width: usize,
+    icon_size: i32,
     applications: Rc<HashMap<Key, VApp>>,
 }
 
@@ -117,7 +157,12 @@ fn activate(application: &gtk4::Application, config: &VConfig) {
     window.add_controller(click_gesture);
     window.add_controller(key);
 
-    let grid = make_application_grid(config.applications.clone(), config.width, stack.clone());
+    let grid = make_application_grid(
+        config.applications.clone(),
+        config.width,
+        stack.clone(),
+        config.icon_size,
+    );
 
     stack.add_child(&grid);
     stack.set_visible_child(&grid);
@@ -132,6 +177,7 @@ fn make_application_grid(
     width: usize,
     // window: &gtk4::ApplicationWindow,
     stack: Stack,
+    icon_size: i32,
 ) -> Widget {
     let grid = Grid::builder()
         .row_spacing(20)
@@ -150,7 +196,7 @@ fn make_application_grid(
         match app2.get(&key) {
             Some(launch) => {
                 println!("launch key");
-                switch_to(launch, &stack2, width);
+                switch_to(launch, &stack2, width, icon_size);
                 Propagation::Stop
             }
             None => Propagation::Proceed,
@@ -163,7 +209,7 @@ fn make_application_grid(
     let mut col = 0i32;
 
     for (key, launch) in applications.iter() {
-        let widget = create_square(*key, launch.clone(), stack.clone(), width);
+        let widget = create_square(*key, launch.clone(), stack.clone(), width, icon_size);
 
         grid.attach(&widget, col, row, 1, 1);
 
@@ -178,51 +224,112 @@ fn make_application_grid(
     grid.into()
 }
 
-fn switch_to(launch: &VApp, stack: &Stack, width: usize) {
+fn switch_to(launch: &VApp, stack: &Stack, width: usize, icon_size: i32) {
     match &launch {
-        VApp::Folder(_, sub) => {
-            let application_grid = make_application_grid(sub.clone(), width, stack.clone());
+        VApp::Folder { applications, .. } => {
+            let application_grid =
+                make_application_grid(applications.clone(), width, stack.clone(), icon_size);
             stack.add_child(&application_grid);
             stack.set_visible_child(&application_grid);
         }
-        VApp::Application(command, _, _) => {
-            println!("running command: {command}");
-            Command::new("sh").arg("-c").arg(command).exec();
-        }
+        VApp::Application { command, .. } => match command() {
+            Ok(()) => {}
+            Err(err) => {
+                eprintln!("error launching application: {err}");
+                process::exit(1)
+            }
+        },
     }
 }
 
-fn create_square(key: Key, launch: VApp, stack: Stack, width: usize) -> Widget {
+fn create_square(key: Key, launch: VApp, stack: Stack, width: usize, icon_size: i32) -> Widget {
     let button = Button::new();
 
     let boxo = gtk4::Box::new(Orientation::Vertical, 5);
 
-    let image = Image::from_icon_name(match &launch {
-        VApp::Folder(_, _) => "folder",
-        VApp::Application(_, _, icon) => icon.as_str(),
-    });
+    let image = match &launch {
+        VApp::Folder { image, .. } => image,
+        VApp::Application { image, .. } => image,
+    };
 
-    image.set_icon_size(gtk4::IconSize::Large);
+    // image.set_icon_size(gtk4::IconSize::Large);
+
+    let image = Image::from_paintable(Some(image));
+
+    image.set_pixel_size(icon_size);
 
     boxo.append(&image);
 
     boxo.append(&Label::new(Some(&format!(
         "({}) {}",
         key.name().unwrap(),
+        // launch.name
         match &launch {
-            VApp::Folder(name, _) => name,
-            VApp::Application(_, name, _) => name,
+            VApp::Folder { name, .. } => name,
+            VApp::Application { name, .. } => name,
         }
     ))));
 
     button.set_child(Some(&boxo));
 
-    button.connect_clicked(move |_| switch_to(&launch, &stack, width));
+    button.connect_clicked(move |_| switch_to(&launch, &stack, width, icon_size));
 
     button.into()
 }
 
-fn validate_map(applications: HashMap<String, App>) -> Result<Rc<HashMap<Key, VApp>>> {
+fn fallback<A, F>(prior: Result<A>, f: F) -> Result<A>
+where
+    F: FnOnce() -> Result<A>,
+{
+    match prior {
+        Ok(prior) => Ok(prior),
+        Err(err1) => match f() {
+            Ok(fallback) => Ok(fallback),
+            Err(err) => Err(err).with_context(|| "after failing due to following error: {err1}"),
+        },
+    }
+}
+
+fn load_icon(
+    image: Option<String>,
+    icon: Option<String>,
+    icon_theme: &IconTheme,
+    icon_size: i32,
+) -> Result<Paintable> {
+    match (image, icon) {
+        (None, None) => None.with_context(|| "no image or icon specified"),
+        (None, Some(icon)) => Ok(icon_theme.lookup_icon(&icon, &[], icon_size, 1, TextDirection::Ltr, IconLookupFlags::empty()).into()),
+        (Some(image), None) => Texture::from_filename(&image)
+                .map(|a| a.into())
+                .with_context(|| format!("couldn't load file {image}")),
+        (Some(_), Some(_)) => None.with_context(|| "both image and icon specified, refusing to choose one (wow this program is more indecesive than I am)") ,
+    }
+}
+
+fn get_result_once_cell<'a, A, F>(cell: &'a OnceCell<A>, init: &'a mut F) -> Result<&'a A>
+where
+    F: FnMut() -> Result<A>,
+{
+    match cell.get() {
+        Some(a) => Ok(a),
+        None => {
+            let val = init()?;
+            match cell.set(val) {
+                Ok(_) => {}
+                Err(_) => {
+                    panic!("this should never happen get just returned that it wasn't set")
+                }
+            };
+            Ok(cell.get().unwrap())
+        }
+    }
+}
+
+fn validate_map(
+    applications: HashMap<String, App>,
+    icon_theme: &IconTheme,
+    icon_size: i32,
+) -> Result<Rc<HashMap<Key, VApp>>> {
     Ok(Rc::new(
         applications
             .into_iter()
@@ -230,14 +337,103 @@ fn validate_map(applications: HashMap<String, App>) -> Result<Rc<HashMap<Key, VA
                 Ok((
                     Key::from_name(k).context("failed to parse Key name")?,
                     match v {
-                        App::Folder { name, applications } => {
-                            VApp::Folder(name, validate_map(applications)?)
+                        App::Folder {
+                            name,
+                            applications,
+                            icon,
+                            image,
+                        } => {
+                            let image = match load_icon(image, icon, icon_theme, icon_size) {
+                                Ok(image) => image,
+                                Err(err) => {
+                                    eprintln!("couldn't load icon because of error: {err}");
+                                    icon_theme.lookup_icon("folder", &[], icon_size, 1, TextDirection::Ltr, IconLookupFlags::empty()).into()
+                                }
+                            };
+                            VApp::Folder {
+                                name,
+                                image,
+                                applications: validate_map(applications, icon_theme, icon_size)?,
+                            }
                         }
                         App::Application {
                             command,
                             name,
                             icon,
-                        } => VApp::Application(command, name, icon),
+                            image,
+                            application,
+                        } => {
+                            let has_set_icon = dbg!(!matches!((dbg!(&icon), dbg!(&image)), (None, None)));
+
+
+                            let mut get_application: &mut dyn FnMut() -> Result<DesktopAppInfo> = &mut move || {
+                                let application_name = match &application {
+                                    Some(a) => a,
+                                    None => None.with_context(|| "no application found")?,
+                                };
+
+                                DesktopAppInfo::new(&format!("{application_name}.desktop")).with_context(|| format!("couldn't find desktop file for: {application_name}"))
+                            };
+
+                            let application: OnceCell<DesktopAppInfo> = OnceCell::new();
+
+                            let mut get_application_icon = || -> Result<Paintable> {
+                                let info = get_result_once_cell(&application, &mut get_application)?;
+
+                                let icon = info.icon().with_context(|| "couldn't find icon for application")?.to_string().with_context(|| "could not find icon name for application")?;
+
+                                let icon = icon_theme.lookup_icon(&icon, &[], icon_size, 1, TextDirection::Ltr, IconLookupFlags::empty());
+
+                                Ok(icon.into())
+                            };
+
+                            let image = match if has_set_icon {
+                                match load_icon(image, icon, icon_theme, icon_size) {
+                                    Ok(paintable) => { Ok(paintable) },
+                                    Err(err) => {
+                                        eprintln!("failed to load prefered image due to: {err} attempting to load fallback");
+                                        get_application_icon()
+                                    },
+                                }
+                            } else {
+                                get_application_icon()
+                            } {
+                                Ok(a) => { a },
+                                Err(err) => {
+                                    eprintln!("couldn't load fallback with error: {err}");
+                                    Paintable::new_empty(1, 1)
+                                },
+                            };
+
+                            let command: Rc<dyn Fn() -> Result<()>> = match command {
+                                Some(command) => Rc::new(move || {Command::new("sh").arg("-c").arg(&command).exec(); Ok(())}),
+                                None => {
+                                    let appinfo = get_result_once_cell(&application, &mut get_application)?.clone();
+
+                                    Rc::new(move || {
+                                        appinfo.launch(
+                                            &[],
+                                            Some(
+                                                &Display::default()
+                                                .with_context(|| "failed to open display")?
+                                                .app_launch_context()))
+                                                .with_context(|| "failed to launch application")?;
+                                        process::exit(0);
+                                        }) // AppLaunchContext::new())
+                                    },
+                            };
+
+                            let name = match name {
+                                Some(name) => name,
+                                None => {
+                                    let appinfo = get_result_once_cell(&application, &mut get_application)?.clone();
+
+                                    appinfo.name().to_string()
+                                },
+                            };
+
+                            VApp::Application { name, image, command }
+                        }
                     },
                 ))
             })
@@ -245,14 +441,19 @@ fn validate_map(applications: HashMap<String, App>) -> Result<Rc<HashMap<Key, VA
     ))
 }
 
-fn validate_config(config: Config) -> Result<VConfig> {
+fn validate_config(config: Config, icon_theme: &IconTheme) -> Result<VConfig> {
     Ok(VConfig {
         width: config.width,
-        applications: validate_map(config.applications)?,
+        icon_size: config.icon_size as i32,
+        applications: validate_map(config.applications, icon_theme, config.icon_size as i32)?,
     })
 }
 
 fn main() -> Result<()> {
+    gtk4::init().unwrap();
+
+    let icon_theme = IconTheme::for_display(&Display::default().unwrap());
+
     let Args { css, config } = dbg!(Args::parse());
 
     let config = fs::read_to_string(&config)
@@ -261,7 +462,8 @@ fn main() -> Result<()> {
     let config = serde_json::from_str(&config)
         .with_context(|| format!("failed to parse config (path: {})", config))?;
 
-    let config = validate_config(config).with_context(|| "failed to validate config")?;
+    let config =
+        validate_config(config, &icon_theme).with_context(|| "failed to validate config")?;
 
     let mut flags = ApplicationFlags::empty();
 
